e2fc26c60d4aec9b543034ea3648d85563b12d95
Shamil <shali220@student.liu.se>
2024-04-15 13:38:51 +0200

    merged branches

--- mindle/app/firebaseConfig.ts
+++ mindle/app/firebaseConfig.ts
@@ -3,7 +3,6 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
