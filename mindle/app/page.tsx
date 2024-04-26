0291a0b26892a331b96121a8e6f77ae76763d58b
Anton Bergman <anton.u.bergman@gmail.com>
2024-04-26 11:45:13 +0200

    fix: root route now re-routes to ./sign-in route

--- mindle/app/page.tsx
+++ mindle/app/page.tsx
@@ -1,6 +1,15 @@
+"use client";
+
 import Image from "next/image";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("./sign-in");
+  }, [router]);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
