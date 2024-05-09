8250686bf46e8063746f9b7428572182c02e5661
Anton Bergman <anton.u.bergman@gmail.com>
2024-05-09 09:53:37 +0200

    fix: calculating color algorithm for letter tiles

--- mindle/app/auth/wordle/Box.tsx
+++ mindle/app/auth/wordle/Box.tsx
@@ -9,6 +9,80 @@ interface BoxProps {
   triggerFlipAnimation: boolean;
 }
 
+/**
+ * Calculates the background color for a letter.
+ *
+ * @param {string} targetWord - The word to be guessed.
+ * @param {string} guess - The player's guess.
+ * @param {number} index - The index of the letter in the guess.
+ * @param {boolean} isGuessed - Indicates whether the word has been fully guessed.
+ * @returns {string} - The background color for the letter tile.
+ */
+function calculateBackgroundColor(
+  targetWord: string,
+  guess: string,
+  index: number,
+  isGuessed: boolean
+): string {
+  // Define CSS class names for colors
+  const greenColor: string = "bg-green-700";
+  const yellowColor: string = "bg-yellow-500";
+  const grayNotGuessedColor: string = "bg-neutral-900";
+  const grayIncorrectGuessColor: string = "bg-neutral-700";
+
+  // If the word has not been guessed, return the gray color for unguessed letters
+  if (!isGuessed) {
+    return grayNotGuessedColor;
+  }
+
+  // Initialize an array to hold background colors for each letter tile
+  let colors = Array(targetWord.length).fill(grayIncorrectGuessColor);
+
+  // Iterate over each letter in the guess
+  for (var i = 0; i < targetWord.length; ++i) {
+    // If the guessed letter matches the target letter at the same index, set background color to green
+    if (targetWord[i] === guess[i]) {
+      colors[i] = greenColor;
+    }
+    // If the guessed letter exists in the target word but is in the wrong position, set background color to yellow
+    else if (targetWord.includes(guess[i])) {
+      colors[i] = yellowColor;
+    }
+  }
+
+  // Iterate over each letter in the guess again
+  for (var i = 0; i < targetWord.length; ++i) {
+    // If the color of the letter tile is yellow, indicating a correct letter in the wrong position
+    if (colors[i] == yellowColor) {
+      // Calculate the maximum count of yellow tiles for this letter in the target word
+      const letter = guess[i];
+      const targetCount = targetWord
+        .split("")
+        .filter((ch) => ch == letter).length;
+      const greenCount = Array.from(Array(targetWord.length).keys()).filter(
+        (j) => guess[j] === letter && colors[j] === greenColor
+      ).length;
+      const maxYellowCount = targetCount - greenCount;
+
+      // Count the current number of yellow tiles for this letter
+      let currentYellowCount = 0;
+      for (var j = 0; j < i; ++j) {
+        if (guess[j] == letter && colors[j] === yellowColor) {
+          currentYellowCount += 1;
+        }
+      }
+
+      // If the current count of yellow tiles reaches the maximum allowed, change the color to incorrect gray
+      if (currentYellowCount == maxYellowCount) {
+        colors[i] = grayIncorrectGuessColor;
+      }
+    }
+  }
+
+  // Return the background color for the letter tile at the specified index
+  return colors[index];
+}
+
 export default function Box({
   row,
   col,
@@ -18,62 +92,6 @@ export default function Box({
 }: BoxProps) {
   const [isBoxFrontFacing, setIsBoxFrontFacing] = useState<boolean>(true);
   const { word, currentGuess, gameLoaded } = useWordle();
-
-  function findAllIndexes(word: string, letter: string): Array<number> {
-    const indexes: Array<number> = [];
-    for (let i = 0; i < word.length; i++) {
-      if (word[i] === letter) {
-        indexes.push(i);
-      }
-    }
-    return indexes;
-  }
-
-  const haveMatchingElements = (
-    array1: Array<number>,
-    array2: Array<number>
-  ): boolean => {
-    return array1.some((element) => array2.includes(element));
-  };
-
-  const calculateBackgroundColor = (
-    word: string,
-    guess: string,
-    col: number,
-    isGuessed: boolean
-  ): string => {
-    const greenColor: string = "bg-green-700";
-    const yellowColor: string = "bg-yellow-500";
-    const grayNotGuessedColor: string = "bg-neutral-900";
-    const grayIncorrectGuessColor: string = "bg-neutral-700";
-
-    const guessLetter = guess[col];
-
-    if (!isGuessed) {
-      return grayNotGuessedColor;
-    }
-
-    if (guessLetter === word[col]) {
-      return greenColor;
-    }
-
-    if (word.includes(guessLetter)) {
-      const wordIndexes = findAllIndexes(word, guessLetter);
-      const guessIndexes = findAllIndexes(guess, guessLetter);
-
-      if (haveMatchingElements(wordIndexes, guessIndexes)) {
-        return grayIncorrectGuessColor;
-      }
-
-      const wordOccurrences = wordIndexes.length;
-      for (let i = 0; i < guessIndexes.length; i++) {
-        if (i + 1 <= wordOccurrences && col === guessIndexes[i]) {
-          return yellowColor;
-        }
-      }
-    }
-    return grayIncorrectGuessColor;
-  };
 
   useEffect(() => {
     if (
